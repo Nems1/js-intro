49e1f79b3773352629b1887e5b465f6c33b2c465
Nems1 <nemwel@hotmail.com>
2022-06-22 02:27:09 +0300

    Created Project setup

---
+++ js/intro.js
@@ -0,0 +1,18 @@
+            'use strict'
+
+            // Declare variables             
+            
+            let name = prompt('What are your names?');
+            let height = parseFloat(prompt("What your height?"));
+            let country = prompt('What is your country name?');
+            
+            // Write content on the screen
+            
+            document.writeln(`<main class = 'container'>
+                <h1>
+                    My name is <span> ${name}</span>
+                </h1><br/>
+                <p>
+                    I am  <span>${height}"</span> tall from <span>${country}</span>
+                </p>
+                </main>`);
